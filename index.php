ddbc9c6ad87dd868fd11524f72d8231bc96f20f3
AndreasMadum <andreashenriksen95@live.dk>
2019-03-19 21:44:03 +0100

    Working Grid
    Det virker!

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php         
+
+
+    function duck()
+    {
+        $ducks =
+            [
+                ["name" => "Rubber Duck", "price" => "1.00", "id" => "000001"],
+                ["name" => "Donald Duck", "price" => "2.00", "id" => "000002"],
+                ["name" => "Scrooge McDuck", "price" => "3.00", "id" => "000003"],
+                ["name" => "Mermaid Duck", "price" => "32.95", "id" => "000004"],
+                ["name" => "Arnold Duck", "price" => "12.00", "id" => "000005"],
+                ["name" => "Sunglasses Duck", "price" => "10.00", "id" => "000006"],
+                ["name" => "Donald Trump Duck", "price" => "32.00", "id" => "000007"],
+                ["name" => "Turtle Duck", "price" => "32.00", "id" => "000008"],
+                ["name" => "Bat Duck", "price" => "32.00", "id" => "000009"],
+                ["name" => "Female Duck", "price" => "32.00", "id" => "000010"],
+                ["name" => "Ninja Duck", "price" => "32.00", "id" => "000011"],
+                ["name" => "Standard Duck", "price" => "32.00", "id" => "000012"],
+            ];
+        foreach ($ducks as $duck)
+        {
+            $getDuckImg = "https://banner2.kisspng.com/20180404/wsq/kisspng-rubber-duck-mallard-ernie-big-duck-duck-5ac4d942243ec3.8484753515228501141485.jpg";
+            $duckImg = '<v-img class="duckImg" src="' . $getDuckImg . '"></v-img>';
+            $b = "<br>";
+            $duckInfo = $duck['name'] . $b . $duck['price'] . ' USD$';
+            $duckString = '<v-flex xs3><v-card>' . $duckImg . '<v-card-text class="px-0">' . $duckInfo . '</v-card-text></v-card></v-flex>';
+            echo $duckString;
+        }
+    }
+?>
 <html>
 <head>
     <link rel="stylesheet" href="css/main.css">
@@ -6,58 +37,21 @@
 </head>
 <body>
 <div id="app">
-
-    <v-container grid-list-md text-xs-center>
-        <v-layout row wrap>
-        <v-flex v-for="i in 3" :key="`4${i}`">
-            <v-card dark color="secondary">
-            <v-card-text class="pa-5 ma-5">
-
-            <?php
-        function duckGrid()
-        {
-            $duckData = [
-                ["name" => "Rubber Duck", "price" => "1", "id" => "1"],
-                ["name" => "Donald Duck", "price" => "2", "id" => "2"],
-                ["name" => "Scrooge McDuck", "price" => "3", "id" => "3"],
-                ["name" => "Mermaid Duck", "price" => "32", "id" => "4"],
-                ["name" => "Arnold Duck", "price" => "12", "id" => "5"],
-                ["name" => "Sunglasses Duck", "price" => "10", "id" => "6"],
-                ["name" => "Donald Trump Duck", "price" => "32", "id" => "7"],
-                ["name" => "Turtle Duck", "price" => "32", "id" => "8"],
-                ["name" => "Bat Duck", "price" => "32", "id" => "9"],
-                ["name" => "Female Duck", "price" => "32", "id" => "10"],
-                ["name" => "Ninja Duck", "price" => "32", "id" => "11"],
-                ["name" => "Standard Duck", "price" => "32", "id" => "12"],
-            ];
-            $br = "<br>";
-            foreach ($duckData as $duck)
-            {   
-
-                $getDuckImg = "https://picsum.photos/100/?random";
-                $getDuckUrl = $getDuckImg;
-                $duckImg = '<a class="duckImgAnchor" href="' . $getDuckUrl .'" target="_blank"><img class="duckImg" src="' . $getDuckImg . '"></a>';
-                $duckString = $duckImg . $br . $duck['name'] . ' - ' . $duck['price'] . ' USD'. $br . 'ItemsNumber: ' . $duck['id'] . $br;
-                $duckEcho = '<div class="duckContainer">' . $duckString . '</div>';
-                echo $duckEcho;
-            }
-        }
-
-        duckGrid();
-        ?>
-
-            </v-card-text>
-            </v-card>
-        </v-flex>      
-        </v-layout>
-    </v-container>
+    <v-app dark>
+        <v-container  grid-list-md text-xs-center>
+            <v-layout mt-5 row wrap>
+                <?php duck(); ?>
+            </v-layout>
+        </v-container>
+    </v-app>
 </div>
-  
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
     <script>
         new Vue({ el: '#app' })
     </script>
 </body>
-
 </html>
