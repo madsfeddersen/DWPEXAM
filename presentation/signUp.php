7b119fdb1706d59d4aeda896dabcd035ea371dd3
AndreasMadum <andreashenriksen95@live.dk>
2019-04-04 17:50:24 +0200

    Minor styling update
    css added

--- presentation/signUp.php
+++ presentation/signUp.php
@@ -1,13 +1,61 @@
-<br>
-<h1 class="page-title">
-    <a>Please register to continue</a>
-</h1>
 
-<h4 class="page-sub-title">
-    Already registered? Click <a href="/login">here</a> to login.
-</h4>
-<br>
+
+<v-app id="app">
+    <v-card>
+    <br>
+    <h1 class="page-title page-title-register" >
+        Register
+    </h1>
+    <h4 class="page-title page-sub-title-register">
+        Already registered? Click <a href="/login">here</a> to login.
+    </h4>
+    <br>
 
 <p class="page-text">
-Insert registration form here
 </p>
+</v-card>
+  <v-form v-model="valid">
+    <v-container>
+      <v-layout>
+          
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+</v-app>
