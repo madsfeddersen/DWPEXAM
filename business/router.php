3f35a72869d108e3303b0794d296907f7be5cef9
AndreasMadum <andreashenriksen95@live.dk>
2019-04-10 17:43:49 +0200

    Router comments
    Added comments for router demo

--- business/router.php
+++ business/router.php
@@ -9,20 +9,47 @@ class Router{
 	}
 
 	public function get($route, $file){
+		
+		//Gets route and file path
+		/*
+		echo "Route: ";
+		print_r($route);
+		echo "<br>";
+		echo "File path: ";
+		print_r($file);
+		echo "<br>";
+		*/
+		
 
 		$uri = trim( $this->request, "/" );
 
+		// Removes or trims the "/" from the URI
+		/*
+		print_r($uri);
+		echo " ";
+		*/
+
+
 		$uri = explode("/", $uri);
+
+		// Splits the string at the "/", returns array of strings
+		/*
+		print_r($uri);
+		echo " ";
+		*/
+
 
 		if($uri[0] == trim($route, "/")){
 
 			array_shift($uri);
 			$args = $uri;
-
+			
+			// Require $file string and add .php extension
 			require $file . '.php';
-
+			
 		}
-
+		
 	}
-
+	
+	
 }
--- presentation/master.php
+++ presentation/master.php
@@ -1,36 +1,34 @@
 <?php
-// This is the master file in which our "template" is structured.
-
     include ("business/router.php");
     include (__DIR__ . "/header.php");
-
-
 ?>
-
 <body>
+<?php
+    include (__DIR__ . "/navigation.php");
 
-    <?php
-        include (__DIR__ . "/navigation.php");
 
-/*Router - it is placed in the middle of the includes in this file, -
-in order to get the content and display it before the footer and script.*/
+    // Gets URI
+    $request = $_SERVER['REQUEST_URI'];
 
-        $request = $_SERVER['REQUEST_URI'];
-        $router = new Router($request);
-        $router->get('/', 'presentation/shop');
-        $router->get('home', 'presentation/home');
-        $router->get('shop', 'presentation/shop');
-        $router->get('login', 'presentation/login');
-        $router->get('signUp', 'presentation/signUp');
-        $router->get('about', 'presentation/about');
-        $router->get('faq', 'presentation/faq');
-        $router->get('contact', 'presentation/contact');
-        $router->get('product/', 'presentation/product');
-       
-        include (__DIR__ . "/footer.php");
-        include (__DIR__ . "/scripts.php");    
-    ?>
-   
+    // Instantiates the Router class and parses URI to router ($request is the URI)
+    $router = new Router($request);
+    
+
+    // Routes
+    $router->get('/', 'presentation/shop');
+    $router->get('home', 'presentation/home');
+    $router->get('shop', 'presentation/shop');
+    $router->get('login', 'presentation/login');
+    $router->get('signUp', 'presentation/signUp');
+    $router->get('about', 'presentation/about');
+    $router->get('faq', 'presentation/faq');
+    $router->get('contact', 'presentation/contact');
+    $router->get('product/', 'presentation/product');
+    
+
+    include (__DIR__ . "/footer.php");
+    include (__DIR__ . "/scripts.php");    
+?>
 </body>
 
 
