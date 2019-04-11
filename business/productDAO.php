bf254c0f21f867ed8fb114223507c9c24aebb880
AndreasMadum <andreashenriksen95@live.dk>
2019-04-11 13:34:27 +0200

    last push of the day
    TO THE LIMIT
    
    push it,, eeeh, like you push your girlfriend (:

--- business/productDAO.php
+++ business/productDAO.php
@@ -49,10 +49,10 @@ function displayProductDetails($productID)
             $b = "<br>";
             $duckInfo = $duck['name'] . " " . 'Duck' . $b . $duck['price'] . ' USD$' . $b . $duck['description'];
             
-            $duckString1 = '<v-flex xl6 md6 xs6><a href="/product/' . $duck['id'] . '">' . $duckImg . '</a></v-flex>';
+            $duckString1 = '<v-flex xl2 md2 xs2></v-flex><v-flex xl4 md4 xs4 ><a href="/product/' . $duck['id'] . '">' . $duckImg . '</a></v-flex>';
            
-            $duckString2 = '<v-flex xl6 md6 xs6 pt-5><a href="/product/' . $duck['id'] . '">' .
-            $duckInfo . '</a></v-flex>';
+            $duckString2 = '<v-flex xl4 md4 xs4 pt-5 class="productdetails"><a href="/product/' . $duck['id'] . '">' .
+            $duckInfo . '</a></v-flex><v-flex xl2 md2 xs2></v-flex>';
 
             echo $duckString1 . $duckString2;
         
