3640bf859ad8735892f6edc97cdf1024f4dacb1e
AndreasMadum <andreashenriksen95@live.dk>
2019-05-22 21:46:19 +0200

    21:46

--- business/handleCheckout.php
+++ business/handleCheckout.php
@@ -1,7 +1,4 @@
 <?php
-
-
-
 $postData = $statusMsg = ''; 
 $status = '';
 
@@ -13,23 +10,24 @@ if(isset($_POST['submit']))
         
     // Validate checkout form fields 
     if(!empty($_POST['fullname']) && !empty($_POST['email']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['zip']) && !empty($_POST['cardname']) && !empty($_POST['cardnumber']) && !empty($_POST['expmonth']) && !empty($_POST['expyear']) && !empty($_POST['cvv']))
-        { 
+    { 
             
-            // Validate reCAPTCHA box 
-            if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
-                // Google reCAPTCHA API secret key 
-                $secretKey = '6LchXaEUAAAAAMgUiUcUXKlMcK24aQDWP1MpSwbA'; 
+        // Validate reCAPTCHA box 
+        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+        { 
+            // Google reCAPTCHA API secret key 
+            $secretKey = '6LchXaEUAAAAAMgUiUcUXKlMcK24aQDWP1MpSwbA'; 
                 
-                // Verify the reCAPTCHA response 
-                $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretKey.'&response='.$_POST['g-recaptcha-response']); 
+            // Verify the reCAPTCHA response 
+            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretKey.'&response='.$_POST['g-recaptcha-response']); 
                 
-                // Decode json data 
-                $responseData = json_decode($verifyResponse); 
+            // Decode json data 
+            $responseData = json_decode($verifyResponse); 
                 
-                // If reCAPTCHA response is valid 
-                if($responseData->success){ 
-                    // Posted form data 
-
+            // If reCAPTCHA response is valid 
+            if($responseData->success)
+            { 
+                // Posted form data 
                 $fname = strip_tags(!empty($_POST['fullname'])?$_POST['fullname']:''); 
                 $email = strip_tags(!empty($_POST['email'])?$_POST['email']:''); 
                 $adr = strip_tags(!empty($_POST['address'])?$_POST['address']:''); 
@@ -58,60 +56,58 @@ if(isset($_POST['submit']))
                 VALUES ('$user_id', '$fname', '$email', '$adr',  '$city', '$zip', '$cname', '$ccnum', '$expmonth', '$expyear', '$cvv', '$productName', '$size', '$color', '$quantity', '$price')");
                 $query->execute();
 
-                    // Send email invoice to the user
-                    $to = $email; 
-                    $subject = 'Order confirmation'; 
-                    $htmlContent = " 
-                        <h1>Order details</h1> 
-                        <p><b>Full Name: </b>".$fname."</p> 
-                        <p><b>Email: </b>".$email."</p> 
-                        <p><b>City: </b>".$adr."</p>
-                        <p><b>Zip: </b>".$zip."</p> 
-                        <p><b>Name on Card: </b>".$cname."</p> 
-                        <p><b>Card Number: </b>".$ccnum."</p>
-                        <p><b>Product: </b>".$productName." Duck</p>
-                        <p><b>Size: </b>".$size."</p>
-                        <p><b>Color: </b>".$color."</p>
-                        <p><b>Quantity: </b>".$quantity."</p>
-                        <p><b>Price: </b>".$price."</p>                
-                    "; 
+                // Send email invoice to the user
+                $to = $email; 
+                $subject = 'Order confirmation'; 
+                $htmlContent = " 
+                <h1>Order details</h1> 
+                <p><b>Full Name: </b>".$fname."</p> 
+                <p><b>Email: </b>".$email."</p> 
+                <p><b>City: </b>".$adr."</p>
+                <p><b>Zip: </b>".$zip."</p> 
+                <p><b>Name on Card: </b>".$cname."</p> 
+                <p><b>Card Number: </b>".$ccnum."</p>
+                <p><b>Product: </b>".$productName." Duck</p>
+                <p><b>Size: </b>".$size."</p>
+                <p><b>Color: </b>".$color."</p>
+                <p><b>Quantity: </b>".$quantity."</p>
+                <p><b>Price: </b>".$price."</p>                
+                "; 
                     
-                    // Always set content-type when sending HTML email 
-                    $headers = "MIME-Version: 1.0" . "\r\n"; 
-                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
-                    // More headers 
-                    $headers .= 'From:'.$fname.' <'.$email.'>' . "\r\n"; 
+                // Always set content-type when sending HTML email 
+                $headers = "MIME-Version: 1.0" . "\r\n"; 
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+                // More headers 
+                $headers .= 'From:'.$fname.' <'.$email.'>' . "\r\n"; 
                     
-                    // Send email 
-                    @mail($to,$subject,$htmlContent,$headers); 
+                // Send email 
+                @mail($to,$subject,$htmlContent,$headers); 
                     
-                    $status = 'success'; 
-                    $statusMsg = 'Your order has been created.'; 
-                    $postData = '';
+                $status = 'success'; 
+                $statusMsg = 'Your order has been created.'; 
+                $postData = '';
                     
-                    redirect_to("/orders");
-                }
-                else
-                { 
-                    $statusMsg = 'Robot verification failed, please try again.'; 
-                    echo $statusMsg;
-                    redirect_to("/checkout");
-                } 
+                redirect_to("/orders");
             }
             else
             { 
-                $statusMsg = 'Please check on the reCAPTCHA box.'; 
+                $statusMsg = 'Robot verification failed, please try again.'; 
                 echo $statusMsg;
                 redirect_to("/checkout");
             } 
         }
         else
         { 
-            $statusMsg = 'Please fill in all the mandatory fields.'; 
+            $statusMsg = 'Please check on the reCAPTCHA box.'; 
             echo $statusMsg;
             redirect_to("/checkout");
         } 
+    }
+    else
+    { 
+        $statusMsg = 'Please fill in all the mandatory fields.'; 
+        echo $statusMsg;
+        redirect_to("/checkout");
+    } 
 }
-
-
 ?>
--- persistence/userDAO/createUser.php
+++ persistence/userDAO/createUser.php
@@ -1,18 +1,16 @@
 <?php
-
 // If the form is submitted 
-if(isset($_POST['submit'])){ 
+if(isset($_POST['submit']))
+{ 
     $postData = $_POST; 
-     
-   
-
+    
     // Validate form fields 
-    if(!empty($_POST['userEmail']) && !empty($_POST['userPass']) && !empty($_POST['firstName']) && !empty($_POST['lastName'])  ){ 
+    if(!empty($_POST['userEmail']) && !empty($_POST['userPass']) && !empty($_POST['firstName']) && !empty($_POST['lastName'])  )
+    { 
          
-
-
         // Validate reCAPTCHA box 
-        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
+        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+        { 
             // Google reCAPTCHA API secret key 
             $secretKey = '6LchXaEUAAAAAMgUiUcUXKlMcK24aQDWP1MpSwbA'; 
              
@@ -23,44 +21,38 @@ if(isset($_POST['submit'])){
             $responseData = json_decode($verifyResponse); 
              
             // If reCAPTCHA response is valid 
-            if($responseData->success){ 
-
-                
-
+            if($responseData->success)
+            { 
                 require_once (__DIR__ . "/../../business/dbcon.php");
-                
-
-                    $userEmail = strip_tags($_POST['userEmail']);
-                    $firstName = strip_tags($_POST['firstName']);
-                    $lastName = strip_tags($_POST['lastName']);
-                    $userPass = strip_tags($_POST['userPass']);
-                    $userRank = strip_tags($_POST['userRank']);
+                $userEmail = strip_tags($_POST['userEmail']);
+                $firstName = strip_tags($_POST['firstName']);
+                $lastName = strip_tags($_POST['lastName']);
+                $userPass = strip_tags($_POST['userPass']);
+                $userRank = strip_tags($_POST['userRank']);
 
                 $dbCon = dbCon();
                 $query = $dbCon->prepare("INSERT INTO users (`userEmail`, `userPass`, `firstName`, `lastName`, `userRank`) VALUES ('$userEmail', '$userPass', '$firstName', '$lastName',  '$userRank')");
                 $query->execute();
-                    header("Location: /login");
                 
-
-                
-                }
-                else
-                { 
-                    $statusMsg = 'Robot verification failed, please try again.'; 
-                    //header("Location: /signup");
-                } 
+                header("Location: /login");
             }
             else
             { 
-                $statusMsg = 'Please check on the reCAPTCHA box.'; 
-                //header("Location: /signup");
+                $statusMsg = 'Robot verification failed, please try again.'; 
+                ("Location: /signup");
             } 
         }
         else
         { 
-            $statusMsg = 'Please fill all the mandatory fields.'; 
-            //header("Location: /signup");
+            $statusMsg = 'Please check on the reCAPTCHA box.'; 
+            ("Location: /signup");
         } 
+    }
+    else
+    { 
+        $statusMsg = 'Please fill all the mandatory fields.'; 
+        ("Location: /signup");
+    } 
 } 
 
 ?>
