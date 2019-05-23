bafe37599261d0ebe3636bac8839845761111d29
AndreasMadum <andreashenriksen95@live.dk>
2019-05-23 02:07:57 +0200

    complete db file

--- Temp/DatabaseFiles/DB_duckshop.sql
+++ Temp/DatabaseFiles/DB_duckshop.sql
@@ -53,6 +53,16 @@ CREATE TABLE IF NOT EXISTS orders (
   price VARCHAR(30)
 );
 
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) NOT NULL,
+  `for_duck` varchar(255)  NOT NULL,
+  `uploaded_on` datetime NOT NULL,
+  `status` enum('1','0') NOT NULL DEFAULT '1',
+  PRIMARY KEY (`image_id`)
+);
+
 CREATE VIEW getSite AS SELECT * FROM duck_shop;
 CREATE VIEW productlist AS SELECT * FROM products;
 
@@ -87,3 +97,6 @@ INSERT INTO `products` (`id`, `name`, `code`, `price`, `description`) VALUES
 
 INSERT INTO `orders` (id, `user_id`, fullname, orderEmail, shipping_address, city, zip, cname, ccnum, expmonth, expyear, cvv, productname, quantity, price) VALUES
 (1, 1, 'Ma name', 'email', 'road 32', 'esbjerg', '6710', 'Card namez', '1111-2222-3333-4444', 'November', '1995', '352', "Trump", 2, 30.00);
+
+INSERT INTO `images` (`image_id`, `file_name`, `for_duck`, `uploaded_on`, `status`) VALUES
+(1, 'Grandma.jpeg', '', '2019-05-22 14:28:14', '1');
